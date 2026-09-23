6835056548dc250537c1bb6682507a93a8fb689f
ig.fom057 <krokonee@gmail.com>
2026-09-23 12:50:37 +0200

    Add initial C++ OOP example

--- 01-basic/03-if else/grade.system.cpp
+++ 01-basic/03-if else/grade.system.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
+
     int i = 0;
     string word;
-do {
-    cout << "Enter your score" << "\n";
-    cin >> i;
-    
-if(i >= 90 && i <= 100) {
-    cout << "Excellent" << "\n";
-}
-else if(i >= 75 && i <= 89) {
-    cout << "Good" << "\n";
-}
-else if(i >= 60 && i <= 74) {
-    cout << "Pass" << "\n";
-}
-else if(i <= 59) {
-    cout << "Fail" << "\n";
-}
-else {
-    cout << "Invalid score" << "\n";
-}
-cout << "Do you want to check again?" << "\n";
-cin >> word;
-}
-while (word == "yes");
-return 0;
+
+    // Die do-while-Schleife wird mindestens einmal ausgeführt
+    do {
+
+        // Benutzer nach seiner Punktzahl fragen
+        cout << "Enter your score" << "\n";
+        cin >> i;
+
+        // Überprüfen, ob die Punktzahl zwischen 90 und 100 liegt
+        if (i >= 90 && i <= 100) {
+            cout << "Excellent" << "\n";
+        }
+
+        // Überprüfen, ob die Punktzahl zwischen 75 und 89 liegt
+        else if (i >= 75 && i <= 89) {
+            cout << "Good" << "\n";
+        }
+
+        // Überprüfen, ob die Punktzahl zwischen 60 und 74 liegt
+        else if (i >= 60 && i <= 74) {
+            cout << "Pass" << "\n";
+        }
+
+        // Wenn die Punktzahl 59 oder weniger ist
+        else if (i <= 59) {
+            cout << "Fail" << "\n";
+        }
+
+        // Wenn die Punktzahl außerhalb des gültigen Bereichs liegt
+        else {
+            cout << "Invalid score" << "\n";
+        }
+
+        // Fragen, ob der Benutzer die Prüfung wiederholen möchte
+        cout << "Do you want to check again?" << "\n";
+        cin >> word;
+
+    // Schleife wiederholen, solange der Benutzer "yes" eingibt
+    } while (word == "yes");
+
+    return 0;
 }
---
+++ 07-projects/Calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>  // basic math function
+
+using namespace std;
+
+int main()
+ {
+    //define variables
+	string operation;
+	double number1, number2;
+    
+	//get user input
+	cout << "Enter First number: ";
+	cin >> number1;
+	cout << "Enter Operation (+, -, *, /): ";
+	cin >> operation;
+	cout << "Enter Second number: ";
+	cin >> number2;
+	
+	//if else statement checking entered operation
+	if (operation == "+") {
+		cout << number1 + number2;
+	} 
+	else if (operation == "-") {
+		cout << number1 - number2;
+	}
+	else if (operation == "/") {
+		cout << number1 / number2;
+	}
+    else { 
+        cout << "Not a recognized operation!";
+	}                            
+	return 0;
+}
